46002c8abe3697c498643ea5500fe97bb8931bc9
gadname <gakugaku1114@icloud.com>
2023-10-20 21:06:12 +0900

    Listenerクラスの定義

--- browser-app/ts/EventListener.ts
+++ browser-app/ts/EventListener.ts
@@ -5,3 +5,18 @@ type Listeners = {
         handler: (e: Event) => void
     }
 }
+
+export class EventListener {
+    private readonly listeners: Listeners = {}
+
+    add(listenerId: string, event: string, element: HTMLElement, handler: (e: Event) => void) {
+        this.listeners[listeneId] = {
+            event,
+            element,
+            handler,
+
+        }
+
+        element.addEventListener(event, handler)
+    }
+}
