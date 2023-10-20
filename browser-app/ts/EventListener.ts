212a8d7e7049e70cf308b6080a1b40ae91471672
gadname <gakugaku1114@icloud.com>
2023-10-20 21:29:02 +0900

    removeメソッドの追加

--- browser-app/ts/EventListener.ts
+++ browser-app/ts/EventListener.ts
@@ -7,6 +7,17 @@ type Listeners = {
 }
 
 export class EventListener {
+
+    remove(listenerId: string) {
+        const listener = this.listeners[listenerId]//listenerオブジェクトから指定のIDをlistenerに定義
+    
+        if(!listener) return
+
+        listener.element.removeEventListener(listener.event, listener.handler)
+
+        delete this.listeners[listenerId]
+    
+    }
     private readonly listeners: Listeners = {}
 
     add(listenerId: string, event: string, element: HTMLElement, handler: (e: Event) => void) {
